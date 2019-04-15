7774f59faef3d1d5bc112211b2ea2a3a35cf9ded
Unknown <s1996ch@gmail.com>
2019-04-15 16:00:52 +0430

    Initial website

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -23,22 +22,30 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
+    render={() => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
+            marginTop: 50,
           }}
         >
           <main>{children}</main>
-          <footer>
+          <footer style={{ position: "absolute", bottom: 20 }}>
             © {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a target="_blank" href="https://www.gatsbyjs.org">Gatsby</a>
+            {` `}|{` `}
+            Hosted on
+            {` `}
+            <a target="_blank" href="https://www.github.com">GitHub</a>
+            {` `}|{` `}
+            By
+            {` `}
+            <a target="_blank" href="https://github.com/soroushchehresa">Soroush Chehresa</a>
           </footer>
         </div>
       </>
