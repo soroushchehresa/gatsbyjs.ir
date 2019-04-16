bccc61a1ea33dc135cc2bdbd89745e445c7c0185
Unknown <s1996ch@gmail.com>
2019-04-16 11:43:15 +0430

    Update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `gatsbyjs.ir`,
+    description: `gatsbyjs.ir`,
+    author: `@soroushchehresa`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
